11eb1c13ebba72f7efb628f9c8553ccfe3464444
admin <admin@admins-MacBook-Pro.local>
2019-11-01 08:42:58 -0700

    GET requests for projects completed

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -2,6 +2,42 @@ const express = require('express')
 const db = require('./projectModel.js')
 const router = express.Router()
 
+// TODO : GET ALL PROJECTS
+router.get('/', (req,res) => {
+     db.get()
+     .then(project => {
+          res.status(200).json(project)
+     })
+})
+
+
+// TODO : GET PROJECT BY ID
+router.get('/:id',(req,res) => {
+     db.get(req.params.id)
+     .then(project => {
+          res.status(200).json(project)
+     })
+})
+
+
+// TODO : GET ACTIONS BY PROJECT ID
+router.get('/:id/action',(req,res) => {
+     db.getProjectActions(req.params.id)
+     .then(actions => {
+          res.status(200).json(actions)
+     })
+})
+
+
+
+
+// TODO: POST NEW PROJET
+
+
+
+
+
+// TODO : POST NEW ACTION BY USER ID 
 
 
 
@@ -9,19 +45,13 @@ const router = express.Router()
 
 
 
+// TODO: DELETE USER PROJECT
 
 
 
 
 
-
-
-
-
-
-
-
-
+// TODO: UPDATE USER PROJECT 
 
 
 
