00930a8a1dd12861912f1f64734b6e748b9b4cb4
admin <admin@admins-MacBook-Pro.local>
2019-11-01 09:25:49 -0700

    actionRouter completed

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -10,11 +10,28 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/:id', (req, res) => {
+     actionDB.get(req.params.id)
+     .then(action => {
+          res.status(200).json(action)
+     })
+})
 
 
 
+router.delete('/:id', (req, res) => {
+     actionDB.remove(req.params.id)
+     .then(deleted => {
+          res.status(200).json(deleted)
+     })
+})
 
-
+router.put('/:id', (req, res) => {
+     actionDB.update(req.params.id, req.body)
+          .then(updateAction => {
+               res.status(200).json(updateAction)
+          })
+})
 
 
 
